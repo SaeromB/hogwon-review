c12d7e8565a2545ac8e0323e1966fd9b78d96138
Saerom <saerombang11@gmail.com>
2019-10-25 10:32:25 +0900

    changed CardContainer padding

--- src/Components/Card/CardStyle.js
+++ src/Components/Card/CardStyle.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const CardContainer = styled.div`
-  margin: auto;
-  padding: auto;
+  margin: 2rem;;
+  padding: 3rem 2rem;
   background: white;
   border-radius: 0.5rem;
   box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.16);
